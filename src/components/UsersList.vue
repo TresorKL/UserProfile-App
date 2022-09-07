62b98ccd02db446bd3d570b63ee00973322ad193
TresorKL <trezmathieu@gmail.com>
2022-09-07 11:59:43 +0200

    creating components

---
+++ src/components/UsersList.vue
@@ -0,0 +1,26 @@
+<template>
+  <ion-list>
+    <ion-item
+      v-for="user of users"
+      :router-link="`/detail/${user.id}`"
+      :key="user.id"
+    >
+      <ion-card>
+        <ion-img
+          id="profile-img"
+          :src="user.imageProfile"
+          :key="user.id"
+        ></ion-img>
+        <ion-card-header>
+          <ion-card-title>{{ user.userName }}</ion-card-title>
+          <ion-card-title
+            ><h4 id="prof">Profession:</h4>
+            {{ user.profession }}</ion-card-title
+          >
+        </ion-card-header>
+      </ion-card>
+    </ion-item>
+  </ion-list>
+</template>
+
+<script></script>
---
+++ src/components/UsersListItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <ion-item
+    v-for="user of users"
+    :router-link="`/detail/${user.id}`"
+    :key="user.id"
+  >
+    <ion-card>
+      <ion-img
+        id="profile-img"
+        :src="user.imageProfile"
+        :key="user.id"
+      ></ion-img>
+      <ion-card-header>
+        <ion-card-title>{{ user.userName }}</ion-card-title>
+        <ion-card-title
+          ><h4 id="prof">Profession:</h4>
+          {{ user.profession }}</ion-card-title
+        >
+      </ion-card-header>
+    </ion-card>
+  </ion-item>
+</template>
+<script></script>
